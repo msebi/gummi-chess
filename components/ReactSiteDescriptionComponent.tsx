520e4ced93d8396ce670f5773886f8b77f29dc9b
msebi <mdnssebastian@gmail.com>
2025-12-19 14:43:42 +0100

    Fixed typo

--- components/ReactSiteDescriptionComponent.tsx
+++ components/ReactSiteDescriptionComponent.tsx
@@ -7,7 +7,7 @@ const ReactSiteDescriptionComponent: React.FC = () => {
     <section className="w-full bg-blue-50">
       <div className="container mx-auto flex flex-wrap"> {/* flex-wrap for responsiveness */}
         <div className="w-full md:w-1/2">
-          <ReactDescriptionComponent title="Sharpen Your Chess Skilss Now">
+          <ReactDescriptionComponent title="Sharpen Your Chess Skills Now">
               Whether you&apos;re a pro or just starting out, Gummi&apos;s chess skills are a must have. 
           </ReactDescriptionComponent>
         </div>
